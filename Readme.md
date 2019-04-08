36e3cc9fe65e41bb1722d6378c4e6fad21703083
user <no@mail.com>
2019-04-08 13:49:48 +0200

    IMPROVE docker support.

--- Readme.md
+++ Readme.md
@@ -1,4 +1,5 @@
-= WaLua - Lua web assembly builder
+
+# WaLua - Lua web assembly builder
 
 This script compiles a Web Assembly version of lua.
 
@@ -6,19 +7,37 @@ It downloads the lua sources and Emscripten utilities from the git
 repositories.  However it still depends on some system installed software:
 python, cmake and posix shell and utilities.
 
-Usage:
+# Usage
+
+To build using the software from your os use:
 
 ```
 ./walua.sh make
 ```
 
+Note that emsdk is still downloaded and any other system-wide
+installation is ignored. This method requires python, git and cmake
+installed on your machine.
+
+To build using a container, use:
+
+```
+docker build -t walua
+docker run -v "$PWD:/DATA" walua
+```
+
+This method requires docker installed and running on your machine.
+
+# Output
+
 It generates a `walua_build` subfolder in the current directory containing all
 the files needed to run the PUC-Rio lua VM in a browser. All the sub-folders of
-`walua_build` can be deleted: they are needed at build time only.
+`walua_build` can be deleted: they are needed at build time only. The only release files are: `walua.js` and `walua.wasm`.
 
-In the `walua_build` directory there is also an example html file that let you
-to edit and run lua code: `playground.html`.  It use the
-[ace.js](https://ace.c9.io) editor, that is downloaded from the git repository
-too.  You can run it from the disk, or try the [on-line
+In the `walua_build` directory there is also an example that let
+you to edit and run lua code: `playground.html` plus
+`editor-ace.js`.  It use the [ace.js](https://ace.c9.io) editor,
+that is downloaded from the git repository too.  You can run it
+from the disk, or try the [on-line
 version](https://raw.githack.com/pocomane/walua/master/walua_build/playground.html).
 
--- walua.sh
+++ walua.sh
@@ -1,90 +1,117 @@
 #!/bin/sh
 
+# Needed for container mode: docker
+# Needed for host mode: python, git, cmake
+
 LWDIR="$(dirname $(readlink -f "$0"))"
-
-# Should be installed: python, cmake
-
-if [ "$1" = "" ]; then
-  echo "Build with:"
-  echo "  $0 make"
-  echo "Clear all with:"
-  echo "  $0 clear"
-  exit 0
-fi
-
-if [ "$1" = "clear" ]; then
-  cd "$WORKDIR"
-  rm -fR walua_build/
-  exit 0
-fi
-
-if [ ! "$1" = "make" ]; then
-  exit 0
-fi
 
 mkdir -p walua_build
 cd walua_build
 WORKDIR="$PWD"
 
-cd "$WORKDIR"
-if [ ! -d "emsdk" ]; then
-  PREPARESDK="true"
-fi
-if [ ! -d "lua" ]; then
-  PREPARELUA="true"
-fi
-if [ ! -d "ace-builds" ]; then
-  PREPAREACE="true"
-fi
+prepare_emscripten(){
 
-if [ "$PREPARESDK" = "true" ]; then
   cd "$WORKDIR"
-  git clone https://github.com/emscripten-core/emsdk.git
-  cd emsdk
-  ./emsdk install latest
-  rm ~/.emscripten
-fi
-CLANGDIR="$WORKDIR/emsdk/clang/$(ls $WORKDIR/emsdk/clang)"
-EMDIR="$WORKDIR/emsdk/emscripten/$(ls $WORKDIR/emsdk/emscripten)"
-NODEDIR="$WORKDIR/emsdk/node/$(ls $WORKDIR/emsdk/node)"
+  if [ ! -d "emsdk" ]; then
+    PREPARESDK="true"
+  fi
   
-export PATH="$CLANGDIR:$NODEDIR/bin:$EMDIR:$PATH"
-echo "PATH: $PATH"
+  if [ "$PREPARESDK" = "true" ]; then
+    cd "$WORKDIR"
+    git clone https://github.com/emscripten-core/emsdk.git
+    cd emsdk
+    ./emsdk install latest
+    rm ~/.emscripten
+  fi
+  CLANGDIR="$WORKDIR/emsdk/clang/$(ls $WORKDIR/emsdk/clang)"
+  EMDIR="$WORKDIR/emsdk/emscripten/$(ls $WORKDIR/emsdk/emscripten)"
+  NODEDIR="$WORKDIR/emsdk/node/$(ls $WORKDIR/emsdk/node)"
 
-if [ "$PREPARESDK" = "true" ]; then
+  export PATH="$CLANGDIR:$NODEDIR/bin:$EMDIR:$PATH"
+  echo "PATH: $PATH"
+
+  if [ "$PREPARESDK" = "true" ]; then
+    cd "$WORKDIR"
+    cd emsdk
+    echo '#include <stdio.h>' > main.c
+    echo 'int main(){ printf(""); }' >> main.c
+    emcc main.c
+    rm main.c
+  fi
+}
+
+walua_make() {
+
   cd "$WORKDIR"
-  cd emsdk
-  echo '#include <stdio.h>' > main.c
-  echo 'int main(){ printf(""); }' >> main.c
-  emcc main.c
-  rm main.c
-fi
+  if [ ! -d "lua" ]; then
+    PREPARELUA="true"
+  fi
+  if [ ! -d "ace-builds" ]; then
+    PREPAREACE="true"
+  fi
 
-if [ "$PREPARELUA" = "true" ]; then
+  if [ "$PREPARELUA" = "true" ]; then
+    cd "$WORKDIR"
+    git clone https://github.com/lua/lua
+    cd lua
+    mv onelua.c onelua.c.orig
+    mv lua.c lua.c.origin
+  fi
+
   cd "$WORKDIR"
-  git clone https://github.com/lua/lua
-  cd lua
-  mv onelua.c onelua.c.orig
-  mv lua.c lua.c.origin
-fi
+  echo 'WaLuaSet(Module);' > prejs.js
+  cp "$LWDIR/walua.c" lua/main.c
 
-cd "$WORKDIR"
-echo 'WaLuaSet(Module);' > prejs.js
-cp "$LWDIR/walua.c" lua/main.c
+  if [ "$PREPAREACE" = "true" ]; then
+    cd "$WORKDIR"
+    git clone https://github.com/ajaxorg/ace-builds
+  fi
 
-if [ "$PREPAREACE" = "true" ]; then
   cd "$WORKDIR"
-  git clone https://github.com/ajaxorg/ace-builds
-fi
+  emcc -Os lua/*.c -s EXPORTED_FUNCTIONS="['_run_lua']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" -s MODULARIZE=1 -s 'EXPORT_NAME="WaLua"' --pre-js prejs.js -o walua.js
 
-cd "$WORKDIR"
-emcc -Os lua/*.c -s EXPORTED_FUNCTIONS="['_run_lua']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" -s MODULARIZE=1 -s 'EXPORT_NAME="WaLua"' --pre-js prejs.js -o walua.js
+  cd "$WORKDIR"
+  cat "$WORKDIR/ace-builds/src-min/ace.js" > ./editor-ace.js
+  echo ";" >> ./editor-ace.js
+  cat "$WORKDIR/ace-builds/src-min/mode-lua.js" >> ./editor-ace.js
+  cp "$LWDIR/playground.html" ./
 
-cd "$WORKDIR"
-cat "$WORKDIR/ace-builds/src-min/ace.js" > ./editor-ace.js
-echo ";" >> ./editor-ace.js
-cat "$WORKDIR/ace-builds/src-min/mode-lua.js" >> ./editor-ace.js
-cp "$LWDIR/playground.html" ./
+  cd "$WORKDIR"
+  echo "done:"
+  rm prejs.js
+  ls
+}
 
-rm prejs.js
+cli_parser(){
+
+  if [ "$1" = "" ]; then
+    echo "Build with:"
+    echo "  $0 make"
+    echo "Clear all with:"
+    echo "  $0 clear"
+    exit 0
+  fi
+
+  if [ "$1" = "clear" ]; then
+    cd "$WORKDIR"
+    rm -fR walua_build/
+    exit 0
+  fi
+
+  if [ "$1" = "docker_make" ]; then
+    cd "$WORKDIR"
+    walua_make
+    exit $?
+  fi
+
+  if [ ! "$1" = "make" ]; then
+    exit -1
+  fi
+
+  prepare_emscripten
+  walua_make
+  exit $?
+}
+
+cli_parser $@
 
