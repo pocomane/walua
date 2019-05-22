e2b4f5ec4f4d28217e314a3819f4d7b93e82ce7b
user <no@mail.com>
2019-05-22 09:27:57 +0200

    Re-built with last lua commit (5.4 working d9f40e3f6fb61650240c47d548bee69b24b07859).

--- walua_build/walua.js
+++ walua_build/walua.js
@@ -610,7 +610,7 @@ function updateGlobalBufferViews() {
  Module["HEAPF64"] = HEAPF64 = new Float64Array(buffer);
 }
 
-var DYNAMIC_BASE = 5264e3, DYNAMICTOP_PTR = 20864;
+var DYNAMIC_BASE = 5264048, DYNAMICTOP_PTR = 20912;
 
 var TOTAL_STACK = 5242880;
 
@@ -4578,9 +4578,9 @@ function _getenv(name) {
  return _getenv.ret;
 }
 
-var ___tm_current = 20960;
+var ___tm_current = 21008;
 
-var ___tm_timezone = (stringToUTF8("GMT", 21008, 4), 21008);
+var ___tm_timezone = (stringToUTF8("GMT", 21056, 4), 21056);
 
 function _gmtime_r(time, tmPtr) {
  var date = new Date(HEAP32[time >> 2] * 1e3);
